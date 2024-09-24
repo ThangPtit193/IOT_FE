efe26aa510b5dff711449814152be59a3cd10360
ThangPtit193 <thangpham19032003@gmail.com>
2024-09-24 16:09:57 +0700

    update api historyAction

--- src/pages/Dashboard/Device.tsx
+++ src/pages/Dashboard/Device.tsx
@@ -1,14 +1,26 @@
 import React, { useState, useEffect, CSSProperties } from 'react';
 import { FaFan, FaTv, FaLightbulb, FaToggleOn, FaToggleOff } from 'react-icons/fa';
 import mqtt, { MqttClient } from 'mqtt';
+import { saveHistoryToDatabase } from '../../data/repositories/api';
 
-const Device = () => {
+export interface DeviceSchema {
+  _id: string;
+  name: string;
+  action?: boolean; 
+  createdAt?: Date;
+  updatedAt?: Date;
+}
 
+export interface HistorySchema{
+  deviceId: string;
+  deviceName: string;
+  action:boolean;
+}
+
+const Device = ({ devices }: { devices: DeviceSchema[] }) => {
   const [client, setClient] = useState<MqttClient | null>(null);
-  const [fanOn, setFanOn] = useState<boolean>(false);
-  const [tvOn, setTvOn] = useState<boolean>(false);
-  const [bulbOn, setBulbOn] = useState<boolean>(false);  // Corrected variable name to bulbOn
-  const [loading, setLoading] = useState<{ [key: string]: boolean }>({ fan: false, tv: false, bulb: false });
+  const [deviceStates, setDeviceStates] = useState<{ [key: string]: boolean }>({});
+  const [loading, setLoading] = useState<{ [key: string]: boolean }>({});
 
   useEffect(() => {
     const host = '667e33c453fe4f028d4486b0f821713b.s1.eu.hivemq.cloud';
@@ -32,23 +44,64 @@ const Device = () => {
       console.log('Device component connected to MQTT broker');
     });
 
+    // return () => {
+    //   mqttClient.end(); // Clean up on unmount
+    // };
+
   }, []);
 
-  const toggleDevice = (device: string, state: boolean) => {
+  useEffect(() => {
+    // Initialize device states based on the devices received from the API
+    const initialStates: { [key: string]: boolean } = {};
+    devices.forEach(device => {
+      initialStates[device._id] = device.action || false; // Default to false if action is undefined
+      setLoading(prev => ({ ...prev, [device._id]: false })); // Initialize loading state
+    });
+    setDeviceStates(initialStates);
+  }, [devices]);
+
+  const toggleDevice = async (deviceId: string, deviceName: string) => {
+    
     if (client) {
-      switch (device) {
-        case 'fan':
-          setFanOn(!state);
-          break;
-        case 'tv':
-          setTvOn(!state);
-          break;
-        case 'bulb':  // Fixed the case name
-          setBulbOn(!state);
-          break;
+      const currentState = deviceStates[deviceId];
+      const newState = !currentState;
+
+      setDeviceStates(prev => ({ ...prev, [deviceId]: newState }));
+      setLoading(prev => ({ ...prev, [deviceId]: true }));
+
+      client.publish(`esp8266/${deviceName}`, JSON.stringify({ state: newState }));
+      console.log(deviceName, newState)
+
+
+          try {
+      const response = await fetch(`http://localhost:3001/api/data/update-device/${deviceId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json', // Thêm header này
+        },
+        body: JSON.stringify({ action: newState }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update device action');
+        
       }
-      client.publish(`esp8266/${device}`, JSON.stringify({ state: !state }));
-      console.log("Dữ liệu đã được gửi thành công");
+
+      const result = await response.json();
+      console.log('Device updated:', result.data);
+      const historyEntry = {
+        deviceId: deviceId,
+        deviceName: deviceName,
+        action: newState,
+      };
+      await saveHistoryToDatabase(historyEntry);
+      
+    } catch (error) {
+      console.error('Error updating device action:', error);
+    } finally {
+      setLoading(prev => ({ ...prev, [deviceId]: false }));
+    }
+
     }
   };
 
@@ -77,53 +130,50 @@ const Device = () => {
     position: 'relative',
   };
 
-  const fanStyle: CSSProperties = {
-    animation: fanOn ? 'spin 2s linear infinite' : 'none',
-  };
-
   return (
-    <div className='row d-flex justify-content-center' style={{ gap: '20px' }}>
-      {/* Fan */}
-      <div style={getContainerStyle(fanOn)}>
-        <h4 style={{color: '#284680'}}>Quạt</h4>
-        <div style={{width: '100px', height:'100px', backgroundColor:'#3E7EF7' , borderRadius: '16px'}} className='d-flex justify-content-center align-items-center'>
-          <FaFan style={{ ...iconStyle, ...fanStyle, color: '#fff' }} />
-        </div>
-        <div
-          onClick={() => toggleDevice('fan', fanOn)}
-          style={{ ...buttonStyle, color: fanOn ? '#fff' : '#6c757d' }}
-        >
-          {loading.fan ? <div className="spinner"></div> : (fanOn ? <FaToggleOn /> : <FaToggleOff />)}
-        </div>
-      </div>
-      
-      {/* TV */}
-      <div style={getContainerStyle(tvOn)}>
-        <h4 style={{color: '#284680'}}>Tivi</h4>
-        <div style={{width: '100px', height:'100px', backgroundColor:'#3E7EF7' , borderRadius: '16px'}} className='d-flex justify-content-center align-items-center'>
-          <FaTv style={{ ...iconStyle, color: '#fff' }} />
-        </div>
-        <div
-          onClick={() => toggleDevice('tv', tvOn)}
-          style={{ ...buttonStyle, color: tvOn ? '#fff' : '#6c757d' }}
-        >
-          {loading.tv ? <div className="spinner"></div> : (tvOn ? <FaToggleOn /> : <FaToggleOff />)}
-        </div>
-      </div>
-      
-      {/* Light Bulb */}
-      <div style={getContainerStyle(bulbOn)}>  {/* Updated the style reference */}
-        <h4 style={{color: '#284680'}}>Bóng đèn</h4>
-        <div style={{width: '100px', height:'100px', backgroundColor:'#3E7EF7' , borderRadius: '16px'}} className='d-flex justify-content-center align-items-center'>
-          <FaLightbulb style={{ ...iconStyle, color: '#fff' }} />
-        </div>
-        <div
-          onClick={() => toggleDevice('bulb', bulbOn)}  
-          style={{ ...buttonStyle, color: bulbOn ? '#fff' : '#6c757d' }}
-        >
-          {loading.bulb ? <div className="spinner"></div> : (bulbOn ? <FaToggleOn /> : <FaToggleOff />)}
-        </div>
-      </div>
+    <div>
+      {devices.map((device) => {
+        let Icon;
+        switch (device.name.toLowerCase()) {
+          case 'fan':
+            Icon = FaFan;
+            break;
+          case 'tv':
+            Icon = FaTv;
+            break;
+          case 'bulb':
+            Icon = FaLightbulb;
+            break;
+          default:
+            Icon = FaFan; // Default icon
+        }
+        return (
+          <div style={getContainerStyle(deviceStates[device._id])} key={device._id}>
+            <h4 style={{ color: '#284680' }}>{device.name}</h4>
+            <div
+              style={{
+                width: '100px',
+                height: '100px',
+                backgroundColor: '#3E7EF7',
+                borderRadius: '16px'
+              }}
+              className='d-flex justify-content-center align-items-center'
+            >
+              <Icon style={{ ...iconStyle, color: '#fff' }} />
+            </div>
+            <div
+              onClick={() => toggleDevice(device._id, device.name)}
+              style={{ ...buttonStyle, color: deviceStates[device._id] ? '#fff' : '#6c757d' }}
+            >
+              {loading[device._id] ? (
+                <div className="spinner"></div>
+              ) : (
+                deviceStates[device._id] ? <FaToggleOn /> : <FaToggleOff />
+              )}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,101 +1,57 @@
+// Dashboard.tsx
 import React, { useState, useEffect } from 'react';
 import Overview from './Overview';
 import Chart from './Chart';
-import Device from './Device';
-import mqtt from 'mqtt';
-
-const host = '667e33c453fe4f028d4486b0f821713b.s1.eu.hivemq.cloud'; // ví dụ: 1234abcd.s2.eu.hivemq.cloud
-const port = 8884;
-const clientId = `mqttjs_${Math.random().toString(16).substr(2, 8)}`;
-const connectUrl = `wss://${host}:${port}/mqtt`;
-const topicSensor = "Data_Sensor";
-const topicLight = "Light";
-
-const options = {
-  clientId,
-  username: 'songcuon123',
-  password: 'songcuon123',
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
-};
-
-interface SensorData {
-  temperature: number | null;
-  humidity: number | null;
-  light: number | null;
-}
-
-// Initialize data with the correct structure
-let data: SensorData = {
-  temperature: null,
-  humidity: null,
-  light: null,
-};
+import Device, { DeviceSchema } from './Device';
+import { initializeMqttClient, SensorData } from '../../data/repositories/mqtt';
 
 const Dashboard = () => {
-
-  const [mqttData, setMqttData] = React.useState<SensorData>({
+  
+  const [mqttData, setMqttData] = useState<SensorData>({
     temperature: null,
     humidity: null,
     light: null
-  })
+  });
 
   const [dataSensor, setDataSensor] = useState<SensorData[]>([]);
 
+  const [devices, setDevices] = useState<DeviceSchema[]>([])
 
   useEffect(() => {
-    const client = mqtt.connect(connectUrl, options);
-    client.on('connect', function() {
-      // Subscribe to topics
-      client.subscribe([topicSensor, topicLight]);
+
+
+    initializeMqttClient((newData) => {
+      setMqttData(newData);
+      setDataSensor((prevData) => [...prevData, newData]);
     });
 
-    client.on('message', async (topic, message) => {
-      try {
-        const payload = JSON.parse(message.toString());
-        if (topic === topicSensor) {
-          // Update data with sensor information
-          data.humidity = payload.humidity;
-          data.temperature = payload.temperature;
+    getDevices();
 
-        } else if (topic === topicLight) {
-          data.light = payload.percent;
-        }
-
-        // Reset data if all values are received
-        if (data.temperature !== null && data.humidity !== null && data.light !== null) {
-          setMqttData({
-            temperature: data.temperature,
-            humidity: data.humidity,
-            light: data.light,
-          });
-          const newDataEntry = { 
-            temperature: data.temperature, 
-            humidity: data.humidity, 
-            light: data.light 
-          };
-          
-          setDataSensor((prevData) => [...prevData, newDataEntry]);
-          data = {
-            temperature: null,
-            humidity: null,
-            light: null
-          };
-        }
-      } catch (error) {
-        console.error('Error parsing message:', error);
-      }
-    });
-
+    
   }, []);
+
+  const getDevices = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/data/get-device');
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      console.log(data); // Hiển thị dữ liệu nhận được
+      setDevices(data.data);
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
 
   return (
     <div>
       <Overview mqttData={mqttData} />
       <div className='d-md-flex'>
         <Chart dataSensor={dataSensor} />
-        <Device/>
+        <Device devices={devices}/>
       </div>
     </div>
   );
